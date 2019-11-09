1c79a182ccf589807eb362ec5adbb4dd310ba99e
Mario Sánchez García <mario.sangar@hotmail.com>
2019-11-09 22:08:33 +0100

    add: prism style file for code highlighting
    Added a CSS file that combines the PrismJS themes of Solarized
    Light and Tomorrow into one file. To be able to switch
    between themes, CSS variables have been used and assigned
    when user switches the blog theme. Besides, two plugins of
    PrismJS have been activated (line highlight and prompt).

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,3 +1,6 @@
 // custom typefaces
 import "typeface-montserrat"
 import "typeface-merriweather"
+require("./src/utils/prism-code-high-theme-dependent.css")
+require("prismjs/plugins/line-numbers/prism-line-numbers.css")
+require("prismjs/plugins/command-line/prism-command-line.css")
